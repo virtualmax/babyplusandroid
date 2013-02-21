def4e1115aeee73b1c97f8e669d450cc72e7cc98
marco <mcatalan@virtualmax.cl>
2013-02-21 00:30:00 -0300

    dd
    ddd

--- src/com/example/babyplus/MainActivity.java
+++ src/com/example/babyplus/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.babyplus;
-
-import org.apache.cordova.DroidGap;
+// com.seroton.phonegap.statuslist;
+ 
+import android.app.Activity;
 import android.os.Bundle;
-import com.phonegap.*;
-import android.view.Menu;
+import org.apache.cordova.*;
+ 
+public class MainActivity extends DroidGap
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
 
-public class MainActivity extends DroidGap {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		super.loadUrl("file:///android_asset/www/index.html");
-	}
+
+//package com.example.babyplus;
+
+//import org.apache.cordova.DroidGap;
+//import android.os.Bundle;
+//import com.phonegap.*;
+//import android.view.Menu;
+
+//public class MainActivity extends DroidGap {
+
+//		@Override
+//		public void onCreate(Bundle savedInstanceState) {
+	//			super.onCreate(savedInstanceState);
+	//			super.loadUrl("file:///android_asset/www/index.html");
+	//		}
 
 //	@Override
 //	public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,4 +39,4 @@ public class MainActivity extends DroidGap {
 //		return true;
 //	}
 
-}
+//}
